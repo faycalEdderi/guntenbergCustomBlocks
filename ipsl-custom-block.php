18d7335ed5e9f0070a3ce7363aaa7a00d1fe2f37
faycal.edderi <faycal.edderi@ipsl.fr>
2021-12-06 11:02:11 +0100

    initialisation of the project

---
+++ ipsl-custom-block.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Ipsl-component
+ * Description:       Custom guntenberg block for ipsl
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Edderi Fayçal for IPSL
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       ipsl-custom-block
+ *
+ * @package           ipsl-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
+ */
+function ipsl_block_ipsl_custom_block_block_init() {
+	register_block_type( __DIR__ );
+}
+add_action( 'init', 'ipsl_block_ipsl_custom_block_block_init' );
